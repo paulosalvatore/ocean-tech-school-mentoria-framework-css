81771a91fba43731530f5f448d643d26ef3661a1
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-07 23:35:41 +0000

    chore: initial files

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    Hello, world!
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
