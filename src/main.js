150d56f15cbb143653221c209c9c9f7c6f705b7d
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-08 00:11:24 +0000

    feat: add example with bootstrap

--- src/main.js
+++ src/main.js
@@ -1,27 +1,14 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div class="w-full h-full bg-slate-950 p-16 flex justify-center gap-8">
-    <div class="p-2 text-white">
-      <div class="text-sm">
-        ⬇️
-        DOWNLOAD
-        <span class="text-slate-400">Mbps</span>
-      </div>
-      <div class="text-center text-4xl">
-        236.12
-      </div>
+  <div class="vh-100 d-flex align-items-center justify-content-center bg-slate-950">
+    <div class="text-center text-white">
+      <span class="fs-6">⬇️ DOWNLOAD <span class="text-slate-400">Mbps</span></span>
+      <div class="fs-1">236.12</div>
     </div>
-
-    <div class="p-2 text-white">
-      <div class="text-sm">
-        ⬆️
-        UPLOAD
-        <span class="text-slate-400">Mbps</span>
-      </div>
-      <div class="text-center text-4xl">
-        157.49
-      </div>
+    <div class="ms-4 text-center text-white">
+      <span class="fs-6">⬆️ UPLOAD <span class="text-slate-400">Mbps</span></span>
+      <div class="fs-1">157.49</div>
     </div>
   </div>
 `
