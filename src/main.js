a91a4de1cc468fc29681ec377dbeab4316dcf91f
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-08 00:00:12 +0000

    feat(main): example with tailwind

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div class="font-bold">
-    Hello, world!
+  <div class="w-full h-full bg-slate-950 p-16 flex justify-center gap-8">
+    <div class="p-2 text-white">
+      <div class="text-sm">
+        ⬇️
+        DOWNLOAD
+        <span class="text-slate-400">Mbps</span>
+      </div>
+      <div class="text-center text-4xl">
+        236.12
+      </div>
+    </div>
+
+    <div class="p-2 text-white">
+      <div class="text-sm">
+        ⬆️
+        UPLOAD
+        <span class="text-slate-400">Mbps</span>
+      </div>
+      <div class="text-center text-4xl">
+        157.49
+      </div>
+    </div>
   </div>
 `
